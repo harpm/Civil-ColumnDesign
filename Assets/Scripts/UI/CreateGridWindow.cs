4f878dc15e68ba8a0cda642f2d8d43dff5ab7504
Arsalan Hadidi <arsalan.hadidi1379@gmail.com>
2022-04-15 09:51:39 +0100

    CreateGridWindow upgraded, remaining Custom windows

--- Assets/Scripts/UI/CreateGridWindow.cs
+++ Assets/Scripts/UI/CreateGridWindow.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Michsky.UI.ModernUIPack;
@@ -39,15 +40,24 @@ public class CreateGridWindow : MonoBehaviour
 
     [SerializeField]
     private Button _customXYBtn;
+    
+    [SerializeField]
+    private Button _customSBtn;
+
+    [SerializeField]
+    private GameObject _errorWindow;
+
+    [SerializeField]
+    private TextMeshProUGUI _errorText;
 
 
     private int _xNo;
     private int _yNo;
     private int _sNo;
 
-    private float[] _xSpaces;
-    private float[] _ySpaces;
-    private float[] _sSpaces;
+    private List<float> _xSpaces;
+    private List<float> _ySpaces;
+    private List<float> _sSpaces;
 
     // Start is called before the first frame update
     void Start()
@@ -80,6 +90,20 @@ public class CreateGridWindow : MonoBehaviour
 
     }
 
+    public void CustomSOn()
+    {
+        _sNoInput.interactable = false;
+        _sSpaceInput.interactable = false;
+        _customSBtn.interactable = true;
+    }
+
+    public void CustomSOff()
+    {
+        _sNoInput.interactable = true;
+        _sSpaceInput.interactable = true;
+        _customSBtn.interactable = false;
+    }
+
     public void OnCustomXY()
     {
         _customXYWindow.enabled = true;
@@ -90,15 +114,92 @@ public class CreateGridWindow : MonoBehaviour
     {
         if (!_customXYSwitch.isOn)
         {
+            if (_xNoInput.text == string.Empty)
+            {
+                ShowError("No x should have numeric value");
+                return;
+            }
 
+            if (_yNoInput.text == string.Empty)
+            {
+                ShowError("No y Should have numeric value");
+                return;
+            }
+
+            if (_xSpaceInput.text == string.Empty)
+            {
+                ShowError("No y Should have numeric value");
+                return;
+            }
+
+            if (_ySpaceInput.text == string.Empty)
+            {
+                ShowError("No y Should have numeric value");
+                return;
+            }
+
+            _xNo = Int32.Parse(_xNoInput.text);
+            _yNo = Int32.Parse(_yNoInput.text);
+            
+            var defaultXSpace = float.Parse(_xSpaceInput.text);
+            _xSpaces.ForEach(f => f = defaultXSpace);
+
+            var defaultYSpace = float.Parse(_ySpaceInput.text);
+            _ySpaces.ForEach(f => f = defaultYSpace);
         }
 
         if (!_customStoriesSwitch.isOn)
         {
+            if (_sNoInput.text == string.Empty)
+            {
+                ShowError("No y Should have numeric value");
+                return;
+            }
 
+            if (_sSpaceInput.text == string.Empty)
+            {
+                ShowError("No y Should have numeric value");
+                return;
+            }
+
+
+            _sNo = Int32.Parse(_sNoInput.text);
+
+            var defaultSSpace = float.Parse(_sSpaceInput.text);
+            _sSpaces.ForEach(f => f = defaultSSpace);
         }
-        
 
-        MainManager.Instance.GridBuilder.GenerateGrid(_xNo, _yNo, _sNo, _xSpaces, _ySpaces, _sSpaces);
+
+        MainManager.Instance.GridBuilder.GenerateGrid(_xNo, _yNo, _sNo, _xSpaces.ToArray(), _ySpaces.ToArray(), _sSpaces.ToArray());
+        Close();
+    }
+
+    private void ShowError(string message)
+    {
+        _errorWindow.SetActive(true);
+        _errorText.text = message;
+    }
+
+    public void DismissError()
+    {
+        _errorWindow.SetActive(false);
+    }
+
+    public void Close()
+    {
+        ResetValues();
+        gameObject.SetActive(false);
+    }
+
+    private void ResetValues()
+    {
+        _xNoInput.text = string.Empty;
+        _yNoInput.text = string.Empty;
+        _sNoInput.text = string.Empty;
+        _xSpaceInput.text = string.Empty;
+        _ySpaceInput.text = string.Empty;
+        _sSpaceInput.text = string.Empty;
+        _customXYSwitch.isOn = false;
+        _customStoriesSwitch.isOn = false;
     }
 }
--- Assets/Scripts/UI/MainWindow.cs
+++ Assets/Scripts/UI/MainWindow.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class MainWindow : MonoBehaviour
 {
+    [SerializeField]
+    private CreateGridWindow _createGridWindow;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void OpenCreatGridWindow()
+    {
+        _createGridWindow.gameObject.SetActive(true);
     }
 }
